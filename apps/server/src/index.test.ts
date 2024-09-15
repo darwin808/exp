4a910bf95ed19d4f3d70bea4ac67d35f60ef75f9
darwin apolinario <darwin.apolinario12>
2024-09-15 19:42:06 +0800

    added test in server

--- apps/server/src/index.test.ts
+++ apps/server/src/index.test.ts
@@ -18,4 +18,53 @@ describe("GET / ", () => {
 
     expect(response.statusCode).toBe(403)
   })
+
+  it("should return a 200 with a valid Bearer token", async () => {
+    const res = await request(app).post("/login").send({ username: "test", password: "test" })
+
+    const token = JSON.parse(res.text).token
+
+    const response = await request(app).get("/").set("Authorization", `Bearer ${token}`)
+
+    expect(response.statusCode).toBe(200)
+  })
+})
+
+describe("GET /verify ", () => {
+  it("should return a 401 since no token", async () => {
+    const response = await request(app).get("/verify")
+    expect(response.statusCode).toBe(401)
+  })
+
+  it("should return a 403 with an invalid Bearer token", async () => {
+    const validToken = "test_token"
+
+    const response = await request(app).get("/verify").set("Authorization", `Bearer ${validToken}`)
+
+    expect(response.statusCode).toBe(403)
+  })
+
+  it("should return a 200 with a valid Bearer token", async () => {
+    const res = await request(app).post("/login").send({ username: "test", password: "test" })
+
+    const token = JSON.parse(res.text).token
+
+    const response = await request(app).get("/verify").set("Authorization", `Bearer ${token}`)
+
+    expect(response.statusCode).toBe(200)
+  })
+})
+
+describe("POST /login", () => {
+  it("should return 200 for test user", async () => {
+    const response = await request(app).post("/login").send({ username: "test", password: "test" })
+
+    expect(response.statusCode).toBe(200)
+  })
+
+  it("should return 401 for invalid user", async () => {
+    const response = await request(app).post("/login").send({ username: "xxx", password: "xxx" })
+
+    expect(response.statusCode).toBe(401)
+  })
 })
