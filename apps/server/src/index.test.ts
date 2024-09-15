38488c1e0b93f71d578969c9ce8ff24135ac88e7
darwin apolinario <darwin.apolinario12>
2024-09-15 19:45:30 +0800

    added test in server

--- apps/server/src/index.test.ts
+++ apps/server/src/index.test.ts
@@ -55,6 +55,32 @@ describe("GET /verify ", () => {
   })
 })
 
+describe("GET /api ", () => {
+  const URL = "/api"
+  it("should return a 401 since no token", async () => {
+    const response = await request(app).get(URL)
+    expect(response.statusCode).toBe(401)
+  })
+
+  it("should return a 403 with an invalid Bearer token", async () => {
+    const validToken = "test_token"
+
+    const response = await request(app).get(URL).set("Authorization", `Bearer ${validToken}`)
+
+    expect(response.statusCode).toBe(403)
+  })
+
+  it("should return a 200 with a valid Bearer token", async () => {
+    const res = await request(app).post("/login").send({ username: "test", password: "test" })
+
+    const token = JSON.parse(res.text).token
+
+    const response = await request(app).get(URL).set("Authorization", `Bearer ${token}`)
+
+    expect(response.statusCode).toBe(200)
+  })
+})
+
 describe("POST /login", () => {
   it("should return 200 for test user", async () => {
     const response = await request(app).post("/login").send({ username: "test", password: "test" })
